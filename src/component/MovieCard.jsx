03c89e723c3dbe03b3550f792742c883a0cd15a0
Fraterne Bahirwe <fraterne2023@gmail.com>
2026-09-06 23:02:02 +0200

    updating my files

---
+++ src/component/MovieCard.jsx
@@ -0,0 +1,45 @@
+import { useMovieContext } from "../contexts/MovieContext";
+import "../css/MovieCard.css";
+
+function MovieCard({ movie }) {
+  const { isFavorite, addToFavorites, removeFromFavorites } = useMovieContext();
+  const favorite = isFavorite(movie.id);
+
+  function onFavoriteClick(e) {
+    e.preventDefault();
+    if (favorite) {
+      removeFromFavorites(movie.id);
+    } else {
+      addToFavorites(movie);
+    }
+  }
+
+  const posterUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path.startsWith('/') ? '' : '/'}${movie.poster_path}`
+    : 'https://via.placeholder.com/500x750?text=No+Poster';
+
+  return (
+    <div className="movie-card">
+      <div className="movie-poster">
+        <img
+          src={posterUrl}
+          alt={movie.title}
+        />
+        <div className="movie-overlay">
+          <button
+            className={`favorite-btn ${favorite ? "active" : ""}`}
+            onClick={onFavoriteClick}
+          >
+            ♥
+          </button>
+        </div>
+      </div>
+      <div className="movie-info">
+        <h3>{movie.title}</h3>
+        <p>{movie.release_date?.split("-")[0]}</p>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard;
